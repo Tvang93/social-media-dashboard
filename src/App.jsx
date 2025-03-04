b801d5a49602a4e6dd6e842e8c29fa0f238a6c35
Tvang93 <vangt39@gmail.com>
2025-03-04 13:47:57 -0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import dashboardComponent from './components/dashboardComponent'
+
+function App() {
+
+  return (
+    <>
+      <dashboardComponent />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/dashboardComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const dashboardComponent = () => {
+
+
+
+  return (
+    <div>
+      <h1 className='text-blue-800'>dashboardComponent</h1>
+    </div>
+  )
+}
+
+export default dashboardComponent
